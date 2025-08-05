0c710975564d623c57f6541d452bd1c72a0b1f5a
jwc20 <choi.jae100@gmail.com>
2025-08-05 15:34:48 +0900

    update example

--- example.py
+++ example.py
@@ -1,18 +1,39 @@
 from bnc import Board, Game, Player
-import random
+from bnc.utils import generate_guess
 
 if __name__ == "__main__":
-    board = Board()
+    _code_length = 4
+    _number_of_colors = 8
+    _secret_code = "1337"
+
+    board = Board(code_length=_code_length, num_of_colors=_number_of_colors)
+
     players = [
         Player("Jae", board.copy()),
         Player("Soo", board.copy()),
+        Player("Ben", board.copy()),
+        Player("Char", board.copy()),
     ]
-    game = Game(players, secret_code="1234")
 
-    # players[0].make_guess("1234")
+    game = Game(players, secret_code=_secret_code)
 
+    game.submit_guess(game.players[0], "1111")
+    game.submit_guess(game.players[0], "1112")  # cannot guess since the game is over
     print(players[0].board.display_board())
 
     for i in range(10):
-        game.submit_guess(game.players[1], str(random.randint(1000, 9999)))
+        game.submit_guess(
+            game.players[1], generate_guess(_code_length, _number_of_colors)
+        )
+        game.submit_guess(
+            game.players[2], generate_guess(_code_length, _number_of_colors)
+        )
+        game.submit_guess(
+            game.players[3], generate_guess(_code_length, _number_of_colors)
+        )
+
     print(players[1].board.display_board())
+    print(players[2].board.display_board())
+    print(players[3].board.display_board())
+
+    print(game.winner.name)
