8d7b50a68fc80b0985f90126b2d7df6b9eb50fca
jwc20 <choi.jae100@gmail.com>
2025-08-05 16:45:36 +0900

    update

--- example.py
+++ example.py
@@ -3,8 +3,8 @@ from bnc.utils import generate_guess
 
 if __name__ == "__main__":
     _code_length = 4
-    _number_of_colors = 8
-    _secret_code = "1337"
+    _number_of_colors = 3
+    _secret_code = "1222"
 
     board = Board(code_length=_code_length, num_of_colors=_number_of_colors)
 
@@ -16,10 +16,10 @@ if __name__ == "__main__":
     ]
 
     game = Game(players, secret_code=_secret_code)
-
-    game.submit_guess(game.players[0], "1111")
-    game.submit_guess(game.players[0], "1112")  # cannot guess since the game is over
-    print(players[0].board.display_board())
+    print(game.state)
+    print("###############")
+    print("###############")
+    print(" ")
 
     for i in range(10):
         game.submit_guess(
@@ -32,8 +32,43 @@ if __name__ == "__main__":
             game.players[3], generate_guess(_code_length, _number_of_colors)
         )
 
-    print(players[1].board.display_board())
-    print(players[2].board.display_board())
-    print(players[3].board.display_board())
+    print(game.state)
 
-    print(game.winner.name)
+    game.submit_guess(game.players[0], "1222")
+    game.submit_guess(game.players[0], "1112")  # cannot guess since the game is over
+
+    print(game.state)
+
+    print(f"player: {game.players[0].name}")
+    players[0].board.display_board()
+    print(game.players[0].game_won)
+    print(game.players[0].game_over)
+    print("###############")
+    print(" ")
+
+    print(f"player: {game.players[1].name}")
+    players[1].board.display_board()
+    print(game.players[1].game_won)
+    print(game.players[1].game_over)
+    print("###############")
+    print(" ")
+
+    print(f"player: {game.players[2].name}")
+    players[2].board.display_board()
+    print("###############")
+    print(game.players[2].game_won)
+    print(game.players[2].game_over)
+    print(" ")
+
+    print(f"player: {game.players[3].name}")
+    players[3].board.display_board()
+    print(game.players[3].game_won)
+    print(game.players[3].game_over)
+    print("###############")
+    print(" ")
+
+    if game.winners:
+        for player in game.winners:
+            print(player.name)
+
+    print(game.state)
