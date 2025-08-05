92e4bf1d144eb738f71aa2755ba385e699ddf14a
jwc20 <choi.jae100@gmail.com>
2025-08-05 10:55:40 +0900

    add board attributes and init board, display board methods

--- bnc/board.py
+++ bnc/board.py
@@ -1,3 +1,20 @@
 class Board:
-    def __init__(self) -> None:
-        pass
+    def __init__(
+        self,
+        code_length: int = 4,
+        num_of_colors: int = 6,
+        num_of_guesses: int = 10,
+    ) -> None:
+        self._code_length = code_length
+        self._num_of_colors = num_of_colors
+        self._num_of_guesses = num_of_guesses
+        self._board = []
+        self._init_board()
+
+    def _init_board(self):
+        for _ in range(self._num_of_guesses):
+            self._board.append([0] * self._code_length)
+
+    def display_board(self):
+        for i, row in enumerate(self._board):
+            print(f"{i}: {row}")
--- example.py
+++ example.py
@@ -2,10 +2,11 @@ from bnc import Board, Player, Game
 
 if __name__ == "__main__":
     board = Board()
+    print(board.display_board())
 
-    players = [
-        Player("Jae", board),
-        Player("Soo", board),
-    ]
-
-    game = Game(players, code="1234")
+    # players = [
+    #     Player("Jae", board),
+    #     Player("Soo", board),
+    # ]
+    #
+    # game = Game(players, code="1234")
