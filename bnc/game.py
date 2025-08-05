c09957b52de66ed30bdba0b4b86d0012a18c6b61
jwc20 <choi.jae100@gmail.com>
2025-08-05 13:31:29 +0900

    add submit_guess() method in Game

--- bnc/game.py
+++ bnc/game.py
@@ -7,6 +7,13 @@ class Game:
         if secret_code:
             self.set_secret_code(secret_code)
 
+    @property
+    def players(self) -> list[Player]:
+        return self._players
+
     def set_secret_code(self, secret_code: str | None) -> None:
         for player in self._players:
             player.board.secret_code = secret_code
+
+    def submit_guess(self, player: Player, guess: str) -> None:
+        player.make_guess(guess)
--- example.py
+++ example.py
@@ -1,4 +1,5 @@
 from bnc import Board, Game, Player
+import random
 
 if __name__ == "__main__":
     board = Board()
@@ -8,9 +9,10 @@ if __name__ == "__main__":
     ]
     game = Game(players, secret_code="1234")
 
-    players[0].make_guess("1234")
+    # players[0].make_guess("1234")
 
     print(players[0].board.display_board())
-    # print(players[0].board.secret_code)
-    # print(players[1].board.secret_code)
+
+    for i in range(10):
+        game.submit_guess(game.players[1], str(random.randint(1000, 9999)))
     print(players[1].board.display_board())
