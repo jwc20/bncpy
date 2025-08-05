0575feaeee90381a26c68d94c5a7d673dd92aed6
jwc20 <choi.jae100@gmail.com>
2025-08-05 11:45:21 +0900

    add Player attributes

--- bnc/player.py
+++ bnc/player.py
@@ -1,3 +1,10 @@
+from bnc import Board
+
+
 class Player:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name: str, board: Board) -> None:
+        self.name = name
+        self.board = board
+
+    def make_guess(self):
+        return
--- example.py
+++ example.py
@@ -4,9 +4,12 @@ if __name__ == "__main__":
     board = Board()
     print(board.display_board())
 
-    # players = [
-    #     Player("Jae", board),
-    #     Player("Soo", board),
-    # ]
+    players = [
+        Player("Jae", board),
+        Player("Soo", board),
+    ]
+
+    print(players[0].board.display_board())
+    print(players[1].board.display_board())
     #
     # game = Game(players, code="1234")
