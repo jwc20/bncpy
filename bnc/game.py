398b450e015b2206dfa525a0a8d9d536a83e0299
jwc20 <choi.jae100@gmail.com>
2025-08-05 12:38:34 +0900

    add players attribute, add set_secret_code() method

--- bnc/game.py
+++ bnc/game.py
@@ -1,3 +1,10 @@
+from .player import Player
+
+
 class Game:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, players: list[Player], secret_code: str | None = None) -> None:
+        self._players = players
+
+    def set_secret_code(self, secret_code: str) -> None:
+        for player in self._players:
+            player.board.set_secret_code(secret_code)
