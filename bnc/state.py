a7d5f5cd10a83fa953f6bf2396eee6a14a261949
jwc20 <choi.jae100@gmail.com>
2025-08-09 13:02:37 +0900

    add file for managing game state

---
+++ bnc/state.py
@@ -0,0 +1,23 @@
+
+from enum import Enum
+import json
+from datetime import datetime
+
+from bncpy import Board, Game, Player
+
+
+
+class GameMode(Enum):
+    SINGLE_PLAYER = 1
+    MULTI_PLAYER = 2
+    
+class GuessEntry:
+    pass
+
+class PlayerState:
+    pass
+
+
+class GameState:
+    pass
+
