0504bc47ef2a09d243e78d56642766b04cf32928
jwc20 <choi.jae100@gmail.com>
2025-08-09 13:21:58 +0900

    add GameConfig data class

--- bnc/state.py
+++ bnc/state.py
@@ -1,23 +1,34 @@
+"""
+https://github.com/faif/python-patterns/blob/master/patterns/behavioral/state.py
+"""
 
+from dataclasses import dataclass
 from enum import Enum
 import json
 from datetime import datetime
 
 from bncpy import Board, Game, Player
-
+from aifc import data
 
 
 class GameMode(Enum):
-    SINGLE_PLAYER = 1
-    MULTI_PLAYER = 2
+    SINGLE_BOARD = 1
+    MULTI_BOARD = 2
     
+@dataclass
+class GameConfig:
+    code_length: int
+    num_of_colors: int
+    num_of_guesses: int
+    
+@dataclass
 class GuessEntry:
     pass
-
+@dataclass
 class PlayerState:
     pass
 
-
+@dataclass
 class GameState:
     pass
 
